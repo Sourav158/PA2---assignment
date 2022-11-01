8f18b61a45b74314cfc553f078bc956880ab479e
Sourav158 <114108771+Sourav158@users.noreply.github.com>
2022-10-31 23:26:59 -0400

    Create pa2Functions.h

---
+++ pa2Functions.h
@@ -0,0 +1,21 @@
+void initialize();
+bool checkCode(char a);
+int factorial(int num);
+int fibonacci(int n);
+double findSqrtValue(double num);
+double naturalLog(double num);
+double areaCircle(double radius);
+double areaSquare(double length);
+int findNextOddValue(int num);
+int findNextEvenValue(int num);
+double findNyanCatValue(double num);
+double doMath(double num, char a);
+double lucky(double num);
+void writeDataToFile(const char *name);
+void readDataFromFile(const char *name);
+
+
+
+
+
+
